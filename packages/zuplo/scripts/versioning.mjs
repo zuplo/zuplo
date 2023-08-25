ac32741af5288c923850c4d9b151d536cee68b36
Nick Chen <nick@zuplo.com>
2023-08-25 16:24:42 -0700

    Change ordering of installation

--- packages/zuplo/scripts/versioning.mjs
+++ packages/zuplo/scripts/versioning.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { read, readFileSync, writeFileSync } from "node:fs";
-import * as shelljs from "shelljs";
+import { readFileSync, writeFileSync } from "node:fs";
+import shelljs from "shelljs";
 import * as prettier from "prettier";
 
 const rawPackage = await readFileSync("package.json", "utf8");
@@ -9,10 +9,9 @@ const packageJson = JSON.parse(rawPackage);
 
 for (const dep in packageJson.dependencies) {
   if (dep.startsWith("@zuplo/")) {
-    const { stdout, stderr, code } = shelljs.default.exec(
-      `npm view ${dep} version`,
-      { silent: true }
-    );
+    const { stdout } = shelljs.exec(`npm view ${dep} version`, {
+      silent: true,
+    });
     packageJson.dependencies[dep] = stdout.trim();
   }
 }

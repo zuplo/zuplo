0e557515b95649d6c85eb0f5835b9f62df81dcdb
Nathan Totten <nathan@ntotten.com>
2023-11-29 13:06:58 -0800

    updated sample

--- examples/route-custom-data/modules/echo.ts
+++ examples/route-custom-data/modules/echo.ts
@@ -4,7 +4,13 @@ export default async function echo(
   request: ZuploRequest,
   context: ZuploContext
 ) {
-  const data = context.route.raw<{ "x-custom": { hello: boolean } }>();
+  const data = context.route.raw<{
+    "x-custom": { hello: boolean };
+    "x-internal": boolean;
+  }>();
   context.log.info(`My custom data: ${data["x-custom"]}`);
+  if (data["x-internal"]) {
+    return data["x-internal"];
+  }
   return data["x-custom"];
 }

c1d5dac9df081c18c5662cb211b6ef1ba0187e02
Nathan Totten <nathan@ntotten.com>
2024-08-27 13:55:26 -0400

    upgraded examples to latest config (#37)

---
+++ scripts/upgrade-examples.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the base directory
+# Get the current working directory
+current_dir=$(pwd)
+
+# Define the relative path
+relative_path="examples"
+
+# Combine them to get the full path
+full_path="$current_dir/$relative_path"
+
+# Loop through each directory in the base directory
+for dir in "$full_path"/*/; do
+  # Check if it is a directory
+  if [ -d "$dir" ]; then
+   npx @zuplo/cli@latest project update -y --dir "$dir"
+  fi
+done

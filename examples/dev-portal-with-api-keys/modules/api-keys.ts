1244fd92469a7af1ea4978c925df6efa65ff9f8b
Martyn Davies <martyn@zuplo.com>
2025-07-30 13:56:12 +0200

    Dev Portal with API Key Creation Example (#57)
    * initial commit
    
    * update example readme page
    
    * readme updates

---
+++ examples/dev-portal-with-api-keys/modules/api-keys.ts
@@ -0,0 +1,35 @@
+import { ZuploContext, ZuploRequest, environment } from "@zuplo/runtime";
+
+const accountName = environment.ZUPLO_ACCOUNT_NAME;
+const bucketName = environment.ZUPLO_API_KEY_SERVICE_BUCKET_NAME;
+
+export default async function (request: ZuploRequest, context: ZuploContext) {
+  const sub = request.user?.sub;
+  const userClaims = request.user?.data;
+  const body = await request.json();
+
+  const response = await fetch(
+    `https://dev.zuplo.com/v1/accounts/${accountName}/key-buckets/${bucketName}/consumers?with-api-key=true`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${environment.ZUPLO_API_KEY}`,
+      },
+      body: JSON.stringify({
+        name: crypto.randomUUID(),
+        managers: [
+          { email: body.metadata?.email ?? "nobody@example.com", sub: sub },
+        ],
+        description: body.description ?? "API Key",
+        tags: {
+          sub: sub,
+          email: body.metadata?.email,
+        },
+        metadata: {},
+      }),
+    }
+  );
+
+  return response.json();
+}
